52a664ee95594aff794da3693864e0e3630fb56f
Tamas Galffy <Tamas_Galffy@epam.com>
2018-01-16 17:39:16 +0100

    Added PixelBuffer

---
+++ src/pixelbuffer.js
@@ -0,0 +1,24 @@
+class PixelBuffer {
+    constructor(context, width, height) {
+        this.width = width
+        this.height = height
+        this.image = context.createImageData(this.width, this.height)
+    }
+
+    set(x,y, r,g,b,a) {
+        x = x|0
+        y = y|0
+        i = (y*this.width + x)*4;
+
+        this.image.data[i+0] = r
+        this.image.data[i+1] = g
+        this.image.data[i+2] = b
+        this.image.data[i+3] = a
+    }
+
+    image() {
+        return this.image
+    }
+}
+
+module.exports = PixelBuffer
